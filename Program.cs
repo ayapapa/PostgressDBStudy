b9de0403198f6e2f3f75b882633c62152cefc193
ayapapa <ayapapajapan@yahoo.co.jp>
2017-10-09 14:27:44 +0900

    初期コミット

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostgressExe
+{
+    // スキーマはMySchema
+    // テーブルEncodeInfo
+    [Table("EncodingInfo", Schema = "MySchema")]
+    class EncodingInfo
+    {
+        [Key] //主キー列の宣言
+        [Column("id")] //列名
+        public int encode_info_id { get; set; }
+
+        [Index(IsUnique = true)]
+        [Column("code_page")]
+        public int CodePage { get; set; }
+
+        [StringLength(80)]
+        [Column("name")]
+        public string Name { get; set; }
+
+        [StringLength(80)]
+        [Column("display_name")]
+        public string DisplayName { get; set; }
+    }
+
+    // テーブルEncodeInfo
+    [Table("AppEncoding", Schema = "MySchema")]
+    class AppEncoding
+    {
+        [Key] //主キー列の宣言
+        [Column("app_encoding_id")] //列名
+        public int Id { get; set; }
+
+        [Index(IsUnique = true)]
+        public int CodePage { get; set; }
+
+        [StringLength(80)]
+        [Column("app_name")]
+        public string AppName { get; set; }
+    }
+
+
+    class MyContext : DbContext
+    {
+        public static string connString = @"Server=localhost;User id=test;Password=testtest;Database=testDb";
+        public MyContext(DbConnection conn)
+        //: base(@"Server=localhost;Port=5432;User Id=test; Password=testtest;Database=testDb")
+        //: base("name=Postgres")
+        //: base(@"Server=localhost; Port=5432;User Id = test; Password=testtest;Database=testDb; providerName=Npgsql")
+        : base(conn, true)
+        {
+        }
+
+        public DbSet<EncodingInfo> EncodingInfos { get; set; }
+        public DbSet<AppEncoding> AppEncoding { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<MyContext>(null);
+            //modelBuilder.HasDefaultSchema("MySchema");
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DbProviderFactory factory = DbProviderFactories.GetFactory("Npgsql");
+            DbConnection conn = factory.CreateConnection();
+            conn.ConnectionString = MyContext.connString;
+            conn.Open();
+            using (var context = new MyContext(conn))
+            {
+                try
+                {
+                    context.EncodingInfos.Add(new EncodingInfo()
+                    {
+                        CodePage = 932,
+                        Name = "shift_jis",
+                        DisplayName = "Japanese (Shift-JIS)"
+                    });
+                    //context.EncodingInfos.Add(new EncodingInfo()
+                    //{
+                    //    CodePage = 20932,
+                    //    Name = "EUC-JP",
+                    //    DisplayName = "Japanese (JIS 0208-1990 and 0212-1990)"
+                    //});
+                    //context.EncodingInfos.Add(new EncodingInfo()
+                    //{
+                    //    CodePage = 65001,
+                    //    Name = "utf-8",
+                    //    DisplayName = "Unicode (UTF-8)"
+                    //});
+
+                    //context.AppEncoding.Add(new AppEncoding()
+                    //{
+                    //    CodePage = 932,
+                    //    AppName = "App-sjis"
+                    //});
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+
+            }
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,66 @@
+﻿# SQLDBTest
+C#でSQLDatabaseを操作してみるプロジェクト
+
+# ビルド準備
+## Postgresをインストール
+　see [The PostgreSQL Global Development Group](https://www.postgresql.org/)
+ 
+## Npgsqlをインストール
+1. プロジェクトファイルをVisualStudioで開く
+2. 「ツールメニュー」→「NuGetパッケージマネージャー」→「パッケージマネージャーコンソール」を開く
+3. コンソールにおいて、```Install-Package npgsql```を実行する
+
+## EntityFrameforkをインストール
+1. プロジェクトファイルをVisualStudioで開く
+2. 「ツールメニュー」→「NuGetパッケージマネージャー」→「パッケージマネージャーコンソール」を開く
+3. コンソールにおいて、```Install-Package npgsql```を実行する
+
+PostgresDB接続文字列：
+@"Server=localhost;Port=5432;User Id=test; Password=testtest;Database=testDb"
+
+EntityFramwork + Npgsql: 問題あり！！解決策あり
+https://stackoverflow.com/questions/40600835/entity-framework-6-npgsql-connection-string-error
+Npgsqlを3.2.5に更新
+EntityFrameworも祭祀版（6.1.3）に更新
+
+EntityFramework利用：
+https://qiita.com/keidrumfreak/items/48df731391174f99b828
+日付ー文字列変換
+https://www.shift-the-oracle.com/sql/functions/to_date.html
+DATE型は比較できる
+http://www.earthlink.co.jp/info/1151/
+
+
+#### Npgsql doesn't support schema creation, so you have to create db manually. Then to avoid this error add this statement somewhere in your code (in your case it might be on the beginning of Main() function):
+https://stackoverflow.com/questions/12545228/npgsql-and-entity-framework-code-first-setup-problems
+
+スキーマ名を指定しよう！
+
+IDはシリアルにして、自動付与させよう！　 （[Index(IsUnique = true)]）識別子指定（[Index(IsUnique = true)]）して識別したいものは設定しよう。
+https://stackoverflow.com/questions/38803122/postgres-error-null-value-in-column-id-during-insert-operation
+
+# Postgresの場合は、DB、スキーマ、Key、Uniqueなどは、作ってあげないといけない
+スキーマ生成スクリプト
+
+```
+-- Table: "MySchema"."EncodingInfo"
+
+-- DROP TABLE "MySchema"."EncodingInfo";
+
+CREATE TABLE "MySchema"."EncodingInfo"
+(
+    name text COLLATE pg_catalog."default" NOT NULL,
+    display_name text COLLATE pg_catalog."default" NOT NULL,
+    code_page integer NOT NULL,
+    id integer NOT NULL DEFAULT nextval('"MySchema"."EncodingInfo_id_seq"'::regclass),
+    CONSTRAINT "EncodingInfo_pkey" PRIMARY KEY (id),
+    CONSTRAINT code_page_unique UNIQUE (code_page)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE "MySchema"."EncodingInfo"
+    OWNER to test;
+```
