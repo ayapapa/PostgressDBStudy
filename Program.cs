82ba6e80ffd5c992e7b67a06967b2cb9cfdb3443
ayapapa <ayapapajapan@yahoo.co.jp>
2020-04-19 17:55:43 +0900

    スタディー追加！

--- Program.cs
+++ Program.cs
@@ -7,114 +7,212 @@ using System.Data.Entity;
 using System.Data.Common;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Npgsql;
 
 namespace PostgressExe
 {
-    // スキーマはMySchema
-    // テーブルEncodeInfo
-    [Table("EncodingInfo", Schema = "MySchema")]
-    class EncodingInfo
-    {
-        [Key] //主キー列の宣言
-        [Column("id")] //列名
-        public int encode_info_id { get; set; }
+    //// スキーマはMySchema
+    //// テーブルEncodeInfo
+    //[Table("EncodingInfo")]
+    //class EncodingInfo
+    //{
+    //    [Key] //主キー列の宣言
+    //    [Column("id")] //列名
+    //    public int encode_info_id { get; set; }
 
-        [Index(IsUnique = true)]
-        [Column("code_page")]
-        public int CodePage { get; set; }
+    //    [Index(IsUnique = true)]
+    //    [Column("code_page")]
+    //    public int CodePage { get; set; }
 
-        [StringLength(80)]
-        [Column("name")]
-        public string Name { get; set; }
+    //    [StringLength(80)]
+    //    [Column("name")]
+    //    public string Name { get; set; }
 
-        [StringLength(80)]
-        [Column("display_name")]
-        public string DisplayName { get; set; }
-    }
+    //    [StringLength(80)]
+    //    [Column("display_name")]
+    //    public string DisplayName { get; set; }
+    //}
 
-    // テーブルEncodeInfo
-    [Table("AppEncoding", Schema = "MySchema")]
-    class AppEncoding
-    {
-        [Key] //主キー列の宣言
-        [Column("app_encoding_id")] //列名
-        public int Id { get; set; }
+    //// テーブルAppEncoding
+    //[Table("AppEncoding")]
+    //class AppEncoding
+    //{
+    //    [Key] //主キー列の宣言
+    //    [Column("app_encoding_id")] //列名
+    //    public int Id { get; set; }
 
-        [Index(IsUnique = true)]
-        public int CodePage { get; set; }
+    //    [Index(IsUnique = true)]
+    //    [Column("code_page")] //列名
+    //    public int CodePage { get; set; }
 
-        [StringLength(80)]
-        [Column("app_name")]
-        public string AppName { get; set; }
-    }
-
+    //    [StringLength(80)]
+    //    [Column("app_name")]
+    //    public string AppName { get; set; }
+    //}
 
     class MyContext : DbContext
     {
         public static string connString = @"Server=localhost;User id=test;Password=testtest;Database=testDb";
-        public MyContext(DbConnection conn)
-        //: base(@"Server=localhost;Port=5432;User Id=test; Password=testtest;Database=testDb")
-        //: base("name=Postgres")
-        //: base(@"Server=localhost; Port=5432;User Id = test; Password=testtest;Database=testDb; providerName=Npgsql")
-        : base(conn, true)
+
+        public MyContext(DbConnection conn) : base(conn, false/*コンテキスト破棄時にコネクションは破棄しない*/)
         {
         }
 
+        /// <summary>
+        /// テーブルEncodingInfo。
+        /// </summary>
         public DbSet<EncodingInfo> EncodingInfos { get; set; }
-        public DbSet<AppEncoding> AppEncoding { get; set; }
 
+        /// <summary>
+        /// テーブルEncodingInfo。
+        /// </summary>
+        public DbSet<AppEncoding> AppEncodings { get; set; }
+
+        /// <summary>
+        /// コンテキスト（本クラス）が初期化されたときの呼ばれる関数。
+        /// </summary>
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            Database.SetInitializer<MyContext>(null);
-            //modelBuilder.HasDefaultSchema("MySchema");
+            base.OnModelCreating(modelBuilder);
+
+            // 初回DBアクセス時に呼び出される初期化用クラス（インターフェイス）を設定（というか、初期化無しにした例）
+            //Database.SetInitializer<MyContext>(null);
+            var mb = modelBuilder.HasDefaultSchema("public");
+
+            //var entity = modelBuilder.Entity<EncodingInfo>();
+            //entity.ToTable("EncodingInfo", "MySchema");
+            //entity.HasKey(e => e.encode_info_id);
+            //entity.Property(e => e.encode_info_id)
+            //    .HasColumnName("id");
+            //entity.Property(e => e.CodePage)
+            //    .HasColumnName("code_page");
+            //entity.Property(e => e.DisplayName)
+            //    .HasColumnName("display_name");
+            //entity.Property(e => e.Name)
+            //    .HasColumnName("name");
+
+            //var entityA = modelBuilder.Entity<AppEncoding>();
+            //entityA.ToTable("AppEncoding", "MySchema");
+            //entityA.HasKey(e => e.Id);
+            //entityA.Property(e => e.Id)
+            //    .HasColumnName("app_encoding_id");
+            //entityA.Property(e => e.CodePage)
+            //    .HasColumnName("code_page");
+            //entityA.Property(e => e.AppName)
+            //    .HasColumnName("app_name");
+
+            //base.OnModelCreating(modelBuilder);
         }
+    }
+
+    /// <summary>
+    /// コンテキストクラス。
+    /// データベースとテーブル/スキーマクラス定義（ここでは、EncodingInfoとAppEncoding）を橋渡しするためのクラス。
+    /// 本クラスを使い、DBへのクエリ―を実行する。
+    /// </summary>
+    class MyContextE : MyContext
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public MyContextE(DbConnection conn) : base(conn)
+        {
+        }
+
+        /// <summary>
+        /// テーブルEncodingInfo。
+        /// </summary>
+        public DbSet<EncodingInfo> EncodingInfos { get; set; }
+    }
+
+    /// <summary>
+    /// コンテキストクラス。
+    /// データベースとテーブル/スキーマクラス定義（ここでは、EncodingInfoとAppEncoding）を橋渡しするためのクラス。
+    /// 本クラスを使い、DBへのクエリ―を実行する。
+    /// </summary>
+    class MyContextA : MyContext
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public MyContextA(DbConnection conn) : base(conn)
+        {
+        }
+
+        /// <summary>
+        /// テーブルEncodingInfo。
+        /// </summary>
+        public DbSet<AppEncoding> AppEncodings { get; set; }
 
     }
 
+    /// <summary>
+    /// サマリーだぞ！！
+    /// </summary>
     public class Program
     {
         static void Main(string[] args)
         {
-            DbProviderFactory factory = DbProviderFactories.GetFactory("Npgsql");
-            DbConnection conn = factory.CreateConnection();
-            conn.ConnectionString = MyContext.connString;
-            conn.Open();
-            using (var context = new MyContext(conn))
+            try
             {
-                try
+                using (var context = new TestModel())
                 {
-                    context.EncodingInfos.Add(new EncodingInfo()
+                    //context.Database.Delete();
+                    context.Database.CreateIfNotExists();
+                    context.MyEntities.Add(new MyEntity()
                     {
-                        CodePage = 932,
-                        Name = "shift_jis",
-                        DisplayName = "Japanese (Shift-JIS)"
+                        //Id = 123,
+                        Name = "Name0000001"
                     });
-                    //context.EncodingInfos.Add(new EncodingInfo()
-                    //{
-                    //    CodePage = 20932,
-                    //    Name = "EUC-JP",
-                    //    DisplayName = "Japanese (JIS 0208-1990 and 0212-1990)"
-                    //});
-                    //context.EncodingInfos.Add(new EncodingInfo()
-                    //{
-                    //    CodePage = 65001,
-                    //    Name = "utf-8",
-                    //    DisplayName = "Unicode (UTF-8)"
-                    //});
-
-                    //context.AppEncoding.Add(new AppEncoding()
-                    //{
-                    //    CodePage = 932,
-                    //    AppName = "App-sjis"
-                    //});
                     context.SaveChanges();
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error: " + e.Message);
-                }
-
+                //using (var conn = new NpgsqlConnection(MyContext.connString))
+                //{
+                //    conn.Open();
+                //    //// AppInfoテーブル編集
+                //    ////using (var context = new MyContextA(conn))
+                //    //using (var context = new MyContext(conn))
+                //    //{
+                //    //    context.Database.CreateIfNotExists();
+                //    //    var table = context.Set<AppEncoding>();
+                //    //    table.Add(new AppEncoding()
+                //    //    {
+                //    //        CodePage = 932,
+                //    //        AppName = "App-sjis"
+                //    //    });
+                //    //    context.SaveChanges();
+                //    //}
+                //    //// EncodingInfoテーブル編集
+                //    using (var context = new MyContext(conn))
+                //    //using (var context = new MyContextE(conn))
+                //    {
+                //        context.EncodingInfos.Add(new EncodingInfo()
+                //        {
+                //            CodePage = 932,
+                //            Name = "shift_jis",
+                //            DisplayName = "Japanese (Shift-JIS)"
+                //        });
+                //        context.EncodingInfos.Add(new EncodingInfo()
+                //        {
+                //            CodePage = 20932,
+                //            Name = "EUC-JP",
+                //            DisplayName = "Japanese (JIS 0208-1990 and 0212-1990)"
+                //        });
+                //        context.EncodingInfos.Add(new EncodingInfo()
+                //        {
+                //            CodePage = 65001,
+                //            Name = "utf-8",
+                //            DisplayName = "Unicode (UTF-8)"
+                //        });
+                //        context.SaveChanges();
+                //    }
+                //}
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            // 最後に閉じる
         }
     }
 }
---
+++ TestModel.cs
@@ -0,0 +1,36 @@
+﻿namespace PostgressExe
+{
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class TestModel : DbContext
+    {
+        // コンテキストは、アプリケーションの構成ファイル (App.config または Web.config) から 'TestModel' 
+        // 接続文字列を使用するように構成されています。既定では、この接続文字列は LocalDb インスタンス上
+        // の 'PostgressExe.TestModel' データベースを対象としています。 
+        // 
+        // 別のデータベースとデータベース プロバイダーまたはそのいずれかを対象とする場合は、
+        // アプリケーション構成ファイルで 'TestModel' 接続文字列を変更してください。
+        public TestModel()
+            : base("name=TestModel")
+        {
+        }
+
+        // モデルに含めるエンティティ型ごとに DbSet を追加します。Code First モデルの構成および使用の
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=390109 を参照してください。
+
+        public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class MyEntity
+    {
+        [Key] //主キー列の宣言
+        [Column("my_id")] //列名
+        public int Id { get; set; }
+
+        [Index(IsUnique = true)]
+        [Column("my_name")] //列名
+        public string Name { get; set; }
+    }
+}
